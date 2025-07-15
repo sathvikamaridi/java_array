fede5abe1a8a39b6114a82caf290d5b08831ae1a
sathvikamaridi <120960220+sathvikamaridi@users.noreply.github.com>
2025-07-15 20:32:00 +0530

    create array sum calculation utility

---
+++ Array03_Sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Array03_Sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+
+        int sum = 0;
+        for (int num : arr) sum += num;
+
+        System.out.println("Sum of array elements = " + sum);
+        scanner.close();
+    }
+}
