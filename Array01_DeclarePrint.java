cc159c8198bd1d27672ea98440efceb7194e5de5
sathvikamaridi <120960220+sathvikamaridi@users.noreply.github.com>
2025-07-15 20:31:28 +0530

    add basic array declaration and printing example

---
+++ Array01_DeclarePrint.java
@@ -0,0 +1,10 @@
+public class Array01_DeclarePrint {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
