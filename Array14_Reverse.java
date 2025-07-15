3e11cefa0cc63cf7d4e5099c2682220b89f8bcf2
sathvikamaridi <120960220+sathvikamaridi@users.noreply.github.com>
2025-07-15 20:34:46 +0530

    implement in-place array reversal

---
+++ Array14_Reverse.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Array14_Reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+
+        System.out.println("Reversed array:");
+        for (int value : arr) System.out.print(value + " ");
+        sc.close();
+    }
+}
